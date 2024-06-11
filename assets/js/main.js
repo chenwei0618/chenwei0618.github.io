eeeb77dd54834a4acfd7240692aa61d9ba61f090
Chen wei <jim930618@gmail.com>
2024-06-11 17:20:39 +0800

    0611 進度更新 滑鼠動態

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,19 @@
       return document.querySelector(el)
     }
   }
+  /*滑鼠動態*/
+  let cursor = document.getElementById("myCursor");
+  let circle = document.getElementById("circle");
+
+  window.addEventListener("mousemove",function(e){
+    let x = e.clientX;
+    let y = e.clientY;
+    cursor.style.left = x-27+"px";
+    cursor.style.top = y-27+"px";
+    circle.style.left = x-17+"px";
+    circle.style.top = y-17+"px";
+
+  })
 
   /**
    * Easy event listener function
